7d2f2696edfa59b4596582e87ad14a85dc841ab7
小山 <lufanzhong@gmail.com>
2017-06-30 15:36:19 +0800

    Update k3screenctrl.lua

--- files/luci/controller/k3screenctrl.lua
+++ files/luci/controller/k3screenctrl.lua
@@ -3,7 +3,7 @@
 module("luci.controller.k3screenctrl", package.seeall)
 
 function index()
-    if not nixio.fs.access("/usr/bin/k3screenctrl") then
+    if not nixio.fs.access("/etc/config/k3screenctrl") then
         return
     end
     entry({"admin","system","k3screenctrl"}, cbi("k3screenctrl"), _("Screen"),60)
